c077c08496b303553c76f67880fd1e06eb57093f
jwkoller <jonwkoller@gmail.com>
2025-12-28 23:24:20 -0500

    Corrected test project reference and added multiple thread test

--- ConcurrentPriorityQueue.Tests/ConcurrentPriorityQueueTests.cs
+++ ConcurrentPriorityQueue.Tests/ConcurrentPriorityQueueTests.cs
@@ -26,5 +26,47 @@ namespace ConcurrentPriorityQueue.Tests
 
             Assert.AreEqual("three", next);
         }
+
+        [TestMethod]
+        public async Task ItemsWithSamePriorityAreDequeuedFirstInFirstOut()
+        {
+            var queue = new ConcurrentPriorityQueue<string, int>();
+            await queue.Enqueue("less important string", 2);
+            await queue.Enqueue("first important string", 1);
+            await queue.Enqueue("second important string", 1);
+
+            string next = await queue.Dequeue();
+
+            Assert.AreEqual("first important string", next);
+        }
+
+        [TestMethod]
+        public async Task EnqueueItemsFromMultipleThreadsAreCaptured()
+        {
+            var queue = new ConcurrentPriorityQueue<string, int>();
+            List<Task> threads = new List<Task>();
+            int numThreads = 100;
+
+            for (int i = 0; i < numThreads; i++)
+            {
+                threads.Add(Task.Run(async () =>
+                {
+                    int priority = new Random().Next(100);
+                    string threadId = Guid.NewGuid().ToString();
+
+                    await queue.Enqueue($"Thread {threadId}", priority);
+
+                }));
+            }
+
+            await Task.WhenAll(threads);
+
+            int numberQueued = await queue.Count();
+            Assert.AreEqual(numThreads, numberQueued);
+
+            var items = await queue.UnorderedItems();
+            int distinctCount = items.Select(x => x.Item1).Distinct().Count();
+            Assert.AreEqual(numThreads, distinctCount);
+        }
     }
 }
